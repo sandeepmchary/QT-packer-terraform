b2bf7e3299c01380aa81e80cd7ba0fc549668dd3
sandeepmchary <awssandeepchary@gmail.com>
2019-11-28 14:05:42 +0530

    qt1

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "myfirst" {
+  ami = "ami-0f2b4fc905b0bd1f1"
+  instance_type ="t2.micro"
+  key_name = "sam"
+  sercurity_groups = ["launch-wizard-1"]
+}
--- myfirstec2/main.tf
+++ myfirstec2/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0f2b4fc905b0bd1f1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
