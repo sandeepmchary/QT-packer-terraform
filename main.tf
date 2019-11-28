965b95b4f3295a16e2b0b89016f3baff98667bee
sandeepmchary <awssandeepchary@gmail.com>
2019-11-28 14:47:35 +0530

    add

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 resource "aws_instance" "myfirst" {
   ami = "ami-0f2b4fc905b0bd1f1"
   instance_type ="t2.micro"
-  key_name = "/home/google/secret/sam"
+  key_name = "/home/google/secret/sam.pem"
   security_groups = ["launch-wizard-1"]
 }
