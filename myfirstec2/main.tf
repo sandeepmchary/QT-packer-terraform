f0a5c7efe0a1858fbe1896400e9a602b54bcac08
sandeepmchary <awssandeepchary@gmail.com>
2019-11-28 13:35:06 +0530

    added

--- myfirstec2/main.tf
+++ myfirstec2/main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0f2b4fc905b0bd1f1"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags ={
       Name = "my-first"
   }
